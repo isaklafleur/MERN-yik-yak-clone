ee4c35e3f9a943b978284f8baa90d4847ccd2d03
Isak La Fleur <isak.engdahl@gmail.com>
2017-07-25 20:11:01 +0200

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:resource', (req, res, next) => {
+  const resource = req.params.resource
+  res.json({
+    confirmation: 'success',
+    resource
+  });
+})
+
+module.exports = router;
